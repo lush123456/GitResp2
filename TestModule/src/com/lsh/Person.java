af92cef09beb68179a6e98014b5ab9ae4ca1c5b9
lush <2196873749@qq.com>
2022-01-21 16:14:04 +0800

    111

--- TestModule/src/com/lsh/Person.java
+++ TestModule/src/com/lsh/Person.java
@@ -4,4 +4,10 @@ public class Person {
     private int age;
     private String name;
     private double height;
+
+    public Person(int age , String name , double height) {
+        this.age = age;
+        this.name = name;
+        this.height = height;
+    }
 }
